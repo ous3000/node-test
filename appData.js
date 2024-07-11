0b84a7264b774822e77ddeb550be22cc4f5f0645
destroyeraseimprove <ukhria@gmail.com>
2024-07-11 20:08:18 +0200

    commit 2

---
+++ appData.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  // console.log('Time: ', Date.now())
+  next()
+})
+
+router.get('/', async function(req, res) {
+
+
+
+
+  await res.status(200).send('hello!')
+})
+
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 9000;
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
+    next();
+});
+
+const appDataRouter = require('./appData')
+
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb'}))
+app.use(bodyParser.json({limit: '50mb'}))
+//app.use(express.static(__dirname + './../dist'));
+
+console.log('ddd');
+
+app.use('/app-data', appDataRouter)
+
+app.listen(PORT);
